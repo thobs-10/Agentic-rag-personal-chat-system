dc18f23ebb02a5f4bbb4abebd817ed893dd5010c
thobela <thobela.sixpence@gmail.com>
2025-09-10 22:28:52 +0200

    update: changed the code  and the values to match the port and values used by the vector DB

--- src/ingestion/src/config.py
+++ src/ingestion/src/config.py
@@ -1,27 +1,20 @@
-import os
-from dataclasses import dataclass
-from pydantic import BaseModel
-from qdrant_client.models import Distance
 from typing import Literal
 
+from pydantic import BaseModel
 
-@dataclass
+
 class QdrantDBConfig(BaseModel):
     vector_size: int = 284
-    vector_distance: Literal[Distance.COSINE, Distance.DOT, Distance.EUCLID, Distance.MANHATTAN] = (
-        Distance.COSINE
-    )
-    port: int = 8080
+    vector_distance: Literal["Cosine", "Dot", "Euclid", "Manhattan"] = "Cosine"
+    port: int = 6333
     url: str = "localhost"
-    batch_size = 32
+    batch_size: int = 32
 
 
-@dataclass
 class TextSplitter(BaseModel):
     chunk_size: int = 500  # Optimal size for your embedding model and LLM context window
     chunk_overlap: int = 100
 
 
-@dataclass
 class EncodingSentenceModel(BaseModel):
     model_name: str = "all-MiniLM-L6-v2"
